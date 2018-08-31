faaedc61f90898ca1e1ca0ba0a9a4be9fec67cef
ryo <ryo.hang@gmail.com>
2018-08-30 23:43:13 -0400

    add agg function

--- python/demo/mapre/reduce_service.py
+++ python/demo/mapre/reduce_service.py
@@ -1,4 +1,12 @@
-from demo.loader import loadCSVfrom 
+from demo.loader import loadCSVfrom
+from pyspark.sql import DataFrame
+from pyspark.sql import functions as func
+
+def aggregateBlocks(df:DataFrame):
+	df.agg(func.sum("blockCount")).show()
+
 
 df = loadCSVfrom(file_path="/Users/ryo/Downloads/btc.csv")
-print(df.count());
+print("total record count: %d" % (df.count()))
+
+aggregateBlocks(df)
