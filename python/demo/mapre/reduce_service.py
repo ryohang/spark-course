df908c221392bbe970062f4e25f1133ee6860866
ryo <ryo.hang@gmail.com>
2018-09-05 22:44:32 -0400

    add root path

--- python/demo/mapre/reduce_service.py
+++ python/demo/mapre/reduce_service.py
@@ -1,12 +1,17 @@
+import os
+import sys
+
 from demo.loader import loadCSVfrom
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as func
 
+
+
 def aggregateBlocks(df:DataFrame):
 	df.agg(func.sum("blockCount")).show()
 
-
-df = loadCSVfrom(file_path="/Users/ryo/Downloads/btc.csv")
+ROOT_DIR = os.path.split(os.environ['VIRTUAL_ENV'])[0]
+df = loadCSVfrom(file_path=ROOT_DIR+"/data/btc.csv")
 print("total record count: %d" % (df.count()))
 
 aggregateBlocks(df)
